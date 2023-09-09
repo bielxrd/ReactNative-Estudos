cad463abedecf96d4d8d5a7e3eb84c785221db2f
Gabriel Dias <gabriel.diasramos@hotmail.com>
2023-09-09 00:01:37 -0300

    criada alguns exemplos de arrow functions, com textInput

--- estudos-reactnative/App.js
+++ estudos-reactnative/App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { Text, TextInput, View } from 'react-native'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+      <Text> Tudo bem com voces? exemplo app </Text>
     </View>
-  );
+  )
+}
+function exemplo01() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text> Ola mundo - exempo 01</Text>
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => { //exemplo 02
+  const [resposta, setResp] = useState('')
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <Text style = {{fontStyle: "italic"}}> Seja bem vindo ao programa de soma</Text>
+      <TextInput style = {{justifyContent: "center"}}
+      value = {resposta} 
+      onChangeText={setResp}
+       clearTextOnFocus= {true}
+       placeholder='Digite qualquer coisa'
+       />
+      <Text> O texto digitado foi {resposta}</Text>
+    </View>
+  )
+}
+
+const exemplo03 = () => <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}><Text>Ola mundo exemplo 03</Text></View>
+
+const exemplo04 = ({nome = "Gabriel", idade = 19}) => {
+  return(
+    <View style = {{flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <Text> EXEMPLO 04 do {nome}, de idade {idade}</Text>
+    </View>
+  )
+}
+export {App, exemplo01, exemplo03, exemplo04}
+
